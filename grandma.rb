269c16cd9e001b1b70133fb65e058097005454fb
David Zeit <dzeit216@gmail.com>
2018-04-13 21:46:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 
 def speak_to_grandma(phrase)
-  if phrase == "not shouting"
+  if phrase == phrase.downcase
     "HUH?! SPEAK UP SONNY!"
     elsif phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "ALL CAPS"
+    elsif phrase == phrase.upcase
       "NO, NOT SINCE 1938!"
  end
 end
