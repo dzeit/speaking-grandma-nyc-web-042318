5a8a83df2d12f2cf992ac34ac8af4bcd170335c0
David Zeit <dzeit216@gmail.com>
2018-04-13 20:00:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,9 @@ def speak_to_grandma(phrase)
   elsif
     puts "NO, NOT SINCE 1938!"
   else
-    puts
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
