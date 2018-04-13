f1d50b0cfb06db6e4e899a26b8a2258d8c1d1c44
David Zeit <dzeit216@gmail.com>
2018-04-13 21:50:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+  if phrase != phrase.upcase
     "HUH?! SPEAK UP SONNY!"
     elsif phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
